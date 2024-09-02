871a7b238fa25f640bcc4946ea1b090721502ff6
kofaysi <17022173+kofaysi@users.noreply.github.com>
2024-09-02 10:20:08 +0200

    Update zatrolene-hry.cz-dark-theme-background-remover.user.js

--- zatrolene-hry.cz-dark-theme-background-remover.user.js
+++ zatrolene-hry.cz-dark-theme-background-remover.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Remove Background Image and Prevent White Blink
+// @name         Zatrolene Hry: Remove Background Image
 // @namespace    https://github.com/kofaysi/
 // @version      1.6
-// @description  Remove the background image and prevent white blink during page load
+// @description  Remove the specific background image on Zatrolene Hry to prevent white blink and allow user theme choice
 // @author       https://github.com/kofaysi/
 // @match        *://www.zatrolene-hry.cz/*
 // @grant        none
@@ -12,16 +12,18 @@
 (function() {
     'use strict';
 
-    // Function to remove the background image from the body and set a default dark background color
+    // Function to remove the background image from the body
     function removeBodyBackgroundImage() {
         const bodyElement = document.body;
 
         // Remove any background image from the body
         bodyElement.style.backgroundImage = 'none';
+        // Optional: Uncomment the line below to force a specific background color
         //bodyElement.style.background = '#000000'; // Set to a dark background color to prevent white flash
     }
 
-    // Immediately set a dark background color to prevent white flash before DOM is fully loaded
+    // Immediately set a background color to prevent white flash before DOM is fully loaded
+    // Optional: Uncomment the line below to set a default background color
     //document.documentElement.style.background = '#000000'; // Set a dark color for the entire HTML
 
     // Ensure the function runs as soon as possible
