cae91bcfc9cf101cc8a9dc31becbf427bb8d9d99
kofaysi <17022173+kofaysi@users.noreply.github.com>
2024-09-19 11:47:08 +0200

    Update InputModeAdjuster.user.js
    Added selector for address id.

--- InputModeAdjuster.user.js
+++ InputModeAdjuster.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Input Mode Adjuster
 // @namespace    https://github.com/kofaysi/
-// @version      2024-01-11
+// @version      1.6
 // @description  Changes inputmode for input fields with id or placeholder containing "keywords"
 // @author       https://github.com/kofaysi/
 // @match        *://*/*
@@ -11,8 +11,7 @@
 (function() {
     'use strict';
 
-    // Function to change input mode for email, telephone, and postal code fields
-    // see https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/inputmode
+    // Function to change input mode for various fields
     function changeInputModeForVariousFields() {
         // Find all input elements
         const inputs = document.querySelectorAll('input');
@@ -20,18 +19,22 @@
         inputs.forEach(input => {
             // Check if id or placeholder contains 'email', 'e-mail', 'telephone', 'phone', 'tel', or 'postal code'
             const isEmailInput = /e-?mail/i.test(input.id) || /e-?mail/i.test(input.placeholder);
-            const isTelephoneInput = /(phone|tel)/i.test(input.id) || /(phone|tel)/i.test(input.placeholder);
-            const isPostalCodeInput = /(code|PSČ|směřovací)/i.test(input.id) || /(code|PSČ|směřovací)/i.test(input.placeholder) || /(code|PSČ|směřovací)/i.test(input.name);
-            
+            const isTelephoneInput = /(phone|tel|szám)/i.test(input.id) || /(phone|tel|szám)/i.test(input.placeholder);
+            const isPostalCodeInput = /(code|PSČ|směřovací|irányító)/i.test(input.id) || /(code|PSČ|směřovací)/i.test(input.placeholder) || /(code|PSČ|směřovací)/i.test(input.name);
+            const isAddressInput = /(adresa|cím|address)/i.test(input.id);
+
             if (isEmailInput) {
                 // Set inputmode to 'email'
                 input.inputMode = 'email';
             } else if (isTelephoneInput) {
                 // Set inputmode to 'tel'
                 input.inputMode = 'tel';
+            } else if (isAddressInput) {
+                // Set inputmode to 'text' for address fields
+                input.inputMode = 'text';
             } else if (isPostalCodeInput) {
-                // Set inputmode to 'numeric' or 'text' based on your preference
-                input.inputMode = 'numeric'; // or 'text'
+                // Set inputmode to 'numeric' based on your preference
+                input.inputMode = 'numeric';
             }
         });
     }
