0e0742d0449060f415fa6c1d536a9bd978375b5d
kofaysi <17022173+kofaysi@users.noreply.github.com>
2024-10-20 23:26:48 +0200

    Update and rename InputModeAdjuster.user.js to input-mode-adjuster.user.js
    rename and update header

--- input-mode-adjuster.user.js
+++ input-mode-adjuster.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Input Mode Adjuster
 // @namespace    https://github.com/kofaysi/general-userscripts/blob/main/InputModeAdjuster.user.js
-// @version      1.10
+// @version      1.11
 // @description  Changes inputmode for input fields with id or placeholder containing "keywords" only if the type is not specified
 // @author       https://github.com/kofaysi/
 // @match        *://*/*
